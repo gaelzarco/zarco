3378e391afb369d2fd262a7de0ea78d5b0eab3a2
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-19 18:54:04 -0700

    Home = my name 📛

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <div className="mx-auto flex items-center content-center justify-center p-5">
+        <div className="mx-auto flex items-center content-center justify-center p-5 pt-10">
             <h1>Contact</h1>
         </div>
     )
