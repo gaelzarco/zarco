e7da237ec5f4b7c91d38c8db30ae29c8d1b54d34
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-19 18:54:42 -0700

    My name is '/' route 🛣️

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,7 @@ export default function NavBar() {
     <nav className={`${inter.className} sticky top-0 w-full flex items-center content-center justify-between p-2 max-md:p-1 bg-neutral-800/30 backdrop-blur-xl rounded-full`}>
         <Link href='/' className='px-3 hover:text-neutral-800 dark:hover:text-neutral-300 py-2 pr-2 text-md font-semibold'>
             <p>
-                Home
+                Gael
             </p>
         </Link>
 
