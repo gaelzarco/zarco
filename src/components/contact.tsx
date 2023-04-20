692d5a6befc4ee8ebacb07a11db1562e5233e23a
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-19 18:48:46 -0700

    Boilerplate e-mail component 📧

---
+++ src/components/contact.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <div className="mx-auto flex items-center content-center justify-center p-5">
+            <h1>Contact</h1>
+        </div>
+    )
+}
