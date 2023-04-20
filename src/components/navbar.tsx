a12ad2fdd7233ede251c04f1c5c70a888403bc4d
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-19 18:49:18 -0700

    Only including routes that exist 🏧

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ import { LinkedInLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
 export default function NavBar() {
 
   return (
-    <nav className={`${inter.className} sticky top-0 w-full flex items-center content-center justify-between px-4 max-md:p-1 bg-neutral-800/30 backdrop-blur-xl rounded-full p-4`}>
+    <nav className={`${inter.className} sticky top-0 w-full flex items-center content-center justify-between p-2 max-md:p-1 bg-neutral-800/30 backdrop-blur-xl rounded-full`}>
         <Link href='/' className='px-3 hover:text-neutral-800 dark:hover:text-neutral-300 py-2 pr-2 text-md font-semibold'>
             <p>
                 Home
@@ -15,7 +15,6 @@ export default function NavBar() {
         </Link>
 
         <div className={`${inter.className} flex items-center content-center justify-between text-sm`}>
-            <Link href="#about" className="hover:text-neutral-800 dark:hover:text-neutral-300 px-3 py-2 font-medium">About</Link>
             <Link href="#work" className="hover:text-neutral-800 dark:hover:text-neutral-300 px-3 py-2 font-medium">Work</Link>
             <Link href="#contact" className="hover:text-neutral-800 dark:hover:text-neutral-300 px-3 py-2 font-medium">Contact</Link>
             <span>|</span>
