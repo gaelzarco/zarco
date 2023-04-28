a538f3ae7285c7c8323d587f65565f2d5fbe11d6
gael <76548719+gaelzarco@users.noreply.github.com>
2023-04-27 21:41:37 -0700

    Removing unsafe code from depen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import { motion } from 'framer-motion'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
-import { ArrowDownIcon, ArrowRightIcon } from '@radix-ui/react-icons';
+import HorizontalScroll from 'react-scroll-horizontal';
+import { ArrowDownIcon, ArrowRightIcon, ArrowUpIcon } from '@radix-ui/react-icons';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 export default function Home() {
 
+  const [ ProjectsScroll, setProjectsScroll ] = useState<boolean>(true);
   const [currentTime, setCurrentTime] = useState<string>(dayjs().tz('America/Los_Angeles').format('hh:mm A'))
   const flagRef = useRef<HTMLParagraphElement | null>(null)
 
@@ -93,7 +95,7 @@ const handleTimeHoverLeave = () => {
             </p>
           </SlideInDiv>
 
-          <SlideUpDiv delay={2}>
+          <SlideUpDiv delay={1.75}>
             <p className='inline-flex items-center text-2xl max-sm:text-xl hover:cursor-pointer'>
               Go to work&nbsp;
               <ArrowDownIcon />
@@ -101,20 +103,20 @@ const handleTimeHoverLeave = () => {
           </SlideUpDiv>
         </div>
 
-        <div id='work' className='flex flex-wrap items-center justify-between w-full mt-48'>
-            <SlideUpDiv className='w-1/2 max-sm:w-full text-4xl max-md:text-2xl' delay={2.5}>
+        <div id='work' className='flex flex-wrap items-center justify-between w-full mt-48 max-sm:mt-36'>
+            <SlideUpDiv className='w-1/2 max-sm:w-full text-4xl max-md:text-2xl' delay={1.75}>
               <p>
                 Showcase of selected projects and Hackathon submissions
               </p>
             </SlideUpDiv>
             <div className='h-[70px] w-1/2 max-sm:w-full text-sm max-sm:text-xs flex items-center justify-end max-sm:justify-start'>
               <div className='flex flex-col my-auto '>
-                <SlideUpDiv className='inline-flex items-center justify-end' delay={2.5}>
+                <SlideUpDiv className='inline-flex items-center justify-end' delay={1.75}>
                   <p>FEATURING</p>
                   <p className='leading-none border border-white rounded-full ml-2 p-1 px-2'>FULL-STACK</p>
                   <p className='leading-none border border-white rounded-full ml-2 p-1 px-2'>UI/UX</p>
                 </SlideUpDiv>
-                <SlideUpDiv className='inline-flex items-center justify-end max-sm:justify-start' delay={3}>
+                <SlideUpDiv className='inline-flex items-center justify-end max-sm:justify-start mt-2' delay={1.85}>
                   <p>FROM</p>
                   <p className='leading-none border border-white rounded-full ml-2 mr-2 p-1 px-2'>2021</p>
                   <p>TO</p>
@@ -123,8 +125,107 @@ const handleTimeHoverLeave = () => {
               </div>
             </div>
         </div>
+
+        <div className='w-full'>
+          <motion.div 
+            className='mx-auto border-b border-white mt-24 max-sm: mt-12'
+            initial={{ opacity: 0, width: 0 }}
+            animate={{ opacity: 1, width: "97%", marginInline: "auto" }}
+            transition={{ duration: 0.75, delay: 2 }}
+          ></motion.div>
+            <SlideUpDiv className='bg-white px-2 w-max rounded-full text-black text-lg max-sm:text-sm' delay={2.10}>
+              <p>Selected Projects</p>
+            </SlideUpDiv>
+        </div>
+        <HorizontalScroll className='min-w-full'>
+        <div className="flex min-w-[420px] h-[520px]">
+          <img
+            src={"../static/audix-x1_so.png"}
+            className="h-full w-full p-1 object-cover rounded-xl hover:cursor-pointer"
+          />
+        </div>
+
+        <div className="flex min-w-[420px] h-[520px] ms-2">
+          <img
+            src={"../static/moxie-x1_so.png"}
+            className="h-full w-full p-1 object-cover rounded-xl hover:cursor-pointer"
+          />
+        </div>
+
+        <div className="flex min-w-[420px] h-[520px] ms-2">
+          <img
+            src={"../static/briefo-x3_so.png"}
+            className="h-full w-full p-1 object-cover rounded-xl hover:cursor-pointer"
+          />
+        </div>
+
+        <div className="flex min-w-[420px] h-[520px] ms-2">
+          <img
+            src={"../static/audix-x1_so.png"}
+            className="h-full w-full p-1 object-cover rounded-xl hover:cursor-pointer"
+          />
+        </div>
+      </HorizontalScroll>
+
+        <div className='bottom-0 inline-flex items-center justify-between w-full'>
+          <SlideInDiv delay={1.5} className='flex items-center text-2xl max-sm:text-xl'>
+            <p>
+              © 2023
+            </p>
+          </SlideInDiv>
+
+          <SlideUpDiv delay={1.5} className='inline-flex items-center justify-end text-2xl max-sm:text-xl hover:cursor-pointer'>
+            Back to top&nbsp;
+            <ArrowUpIcon />
+          </SlideUpDiv>
+        </div>
       </div>
   )
+}
+
+export function ProjectsScrollDiv() {
+
+  return (
+    // <motion.div
+    //   variants={{
+    //     hidden: { opacity: 0, x: 10 },
+    //     visible: { opacity: 1, x: 0 },
+    //   }}
+    //   initial="hidden"
+    //   animate="visible"
+    //   transition={{ duration: 0.75, delay: 2.45 }}
+    // >
+      <HorizontalScroll className='min-w-full'>
+        <div className="flex min-w-[420px] h-[520px]">
+          <img
+            src={"../static/audix-x1_so.png"}
+            className="h-full w-full p-1 object-cover rounded-xl hover:cursor-pointer"
+          />
+        </div>
+
+        <div className="flex min-w-[420px] h-[520px] ms-2">
+          <img
+            src={"../static/moxie-x1_so.png"}
+            className="h-full w-full p-1 object-cover rounded-xl hover:cursor-pointer"
+          />
+        </div>
+
+        <div className="flex min-w-[420px] h-[520px] ms-2">
+          <img
+            src={"../static/briefo-x3_so.png"}
+            className="h-full w-full p-1 object-cover rounded-xl hover:cursor-pointer"
+          />
+        </div>
+
+        <div className="flex min-w-[420px] h-[520px] ms-2">
+          <img
+            src={"../static/audix-x1_so.png"}
+            className="h-full w-full p-1 object-cover rounded-xl hover:cursor-pointer"
+          />
+        </div>
+      </HorizontalScroll>
+    // </motion.div>
+  );
 }
 
 const SlideInDiv = ({ children, delay, className } : { children: ReactNode, delay?: number, className?: string }) => {
@@ -150,7 +251,7 @@ const SlideUpDiv = ({ children, delay, className } : { children: ReactNode, dela
     className={className}
       variants={{
         hidden: { opacity: 0, y: 10 },
-        visible: { opacity: 1, y: 0 },
+        visible: { opacity: 1, y: -1 },
       }}
       initial="hidden"
       animate="visible"
@@ -160,3 +261,4 @@ const SlideUpDiv = ({ children, delay, className } : { children: ReactNode, dela
     </motion.div>
   );
 };
+
