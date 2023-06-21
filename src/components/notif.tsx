2e4829459f9443a16e0eafe9d8826d993b60f67c
gael <76548719+gaelzarco@users.noreply.github.com>
2023-06-20 20:12:15 -0700

    dualshock implementation - bug fixes

--- src/components/notif.tsx
+++ src/components/notif.tsx
@@ -2,9 +2,12 @@ import { SlideUpDiv } from "./motion";
 
 export default function Notif() {
   return (
-    <SlideUpDiv delay={0.25} className="fixed bottom-0 right-0 left-0 mx-auto w-screen flex items-center justify-center content-center mb-5 z-20">
-      <div className="inline-flex border border-orange-500 bg-orange-500/10 backdrop-blur-xl px-5 py-3 rounded-2xl">
-        <h4 className="animate-pulse">website under construction</h4>
+    <SlideUpDiv
+      delay={0.25}
+      className="fixed bottom-0 self-center flex items-center justify-center content-center mb-5 z-20"
+    >
+      <div className="inline-flex border border-orange-500 bg-orange-500/10 backdrop-blur-xl px-3 py-1 rounded-full">
+        <h4 className="animate-pulse">Website under construction</h4>
       </div>
     </SlideUpDiv>
   );
--- src/components/three/centerpiece.tsx
+++ src/components/three/centerpiece.tsx
@@ -1,40 +1,84 @@
-import { type MutableRefObject, useRef } from "react";
-import type { Mesh } from "three";
-import { useFrame, Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
+import { useRef } from "react";
+import { type PrimitiveProps, useFrame, Canvas } from "@react-three/fiber";
+import { PerspectiveCamera, useGLTF } from "@react-three/drei";
 
-export function Box() {
-  const meshRef = useRef<Mesh>(null);
+// export function Box() {
+//   const meshRef = useRef<Mesh>(null);
+
+//   useFrame(() => {
+//     if (!meshRef.current) return;
+
+//     meshRef.current.rotation.x += 0.005;
+//     meshRef.current.rotation.y += 0.005;
+
+//     if (document.body.clientWidth <= 768) {
+//       meshRef.current.scale.setX(0.5);
+//       meshRef.current.scale.setY(0.5);
+//       meshRef.current.scale.setZ(0.5);
+//       meshRef.current.position.setY(0.8);
+//     } else if (document.body.clientWidth <= 1024) {
+//       meshRef.current.scale.setX(0.8);
+//       meshRef.current.scale.setY(0.8);
+//       meshRef.current.scale.setZ(0.8);
+//       meshRef.current.position.setY(-0.7);
+//     } else {
+//       meshRef.current.scale.setX(1.1);
+//       meshRef.current.scale.setY(1.1);
+//       meshRef.current.scale.setZ(1.1);
+//       meshRef.current.position.setY(0);
+//     }
+//   });
+
+//   return (
+//     <mesh ref={meshRef as MutableRefObject<Mesh>}>
+//       <boxGeometry args={[2, 2, 2]} />
+//       <meshStandardMaterial color="white" />
+//     </mesh>
+//   );
+// }
+
+export function PlaystationDualshock() {
+  const primitiveRef = useRef<PrimitiveProps>();
+  const { scene } = useGLTF("/playstation-dualshock/scene.gltf");
 
   useFrame(() => {
-    if (!meshRef.current) return;
+    if (!primitiveRef.current) return;
 
-    meshRef.current.rotation.x += 0.005;
-    meshRef.current.rotation.y += 0.005;
+    primitiveRef.current.rotation.x += 0.002;
+    primitiveRef.current.rotation.y += 0.001;
 
-    if (document.body.clientWidth <= 768) {
-      meshRef.current.scale.setX(0.5);
-      meshRef.current.scale.setY(0.5);
-      meshRef.current.scale.setZ(0.5);
-      meshRef.current.position.setY(0.8);
+    if (document.body.clientWidth <= 640) {
+      primitiveRef.current.scale.setX(1.25);
+      primitiveRef.current.scale.setY(1.25);
+      primitiveRef.current.scale.setZ(1.25);
+      primitiveRef.current.position.setY(0.7);
+    } else if (document.body.clientWidth <= 768) {
+      primitiveRef.current.scale.setX(1.35);
+      primitiveRef.current.scale.setY(1.35);
+      primitiveRef.current.scale.setZ(1.35);
+      primitiveRef.current.position.setY(0.35);
     } else if (document.body.clientWidth <= 1024) {
-      meshRef.current.scale.setX(0.8);
-      meshRef.current.scale.setY(0.8);
-      meshRef.current.scale.setZ(0.8);
-      meshRef.current.position.setY(-0.7);
+      primitiveRef.current.scale.setX(1.45);
+      primitiveRef.current.scale.setY(1.45);
+      primitiveRef.current.scale.setZ(1.45);
+      primitiveRef.current.position.setY(-0.65);
+    } else if (document.body.clientWidth <= 1280) {
+      primitiveRef.current.scale.setX(1.55);
+      primitiveRef.current.scale.setY(1.55);
+      primitiveRef.current.scale.setZ(1.55);
+      primitiveRef.current.position.setY(-0.4);
     } else {
-      meshRef.current.scale.setX(1.1);
-      meshRef.current.scale.setY(1.1);
-      meshRef.current.scale.setZ(1.1);
-      meshRef.current.position.setY(0);
+      primitiveRef.current.scale.setX(1.65);
+      primitiveRef.current.scale.setY(1.65);
+      primitiveRef.current.scale.setZ(1.65);
+      primitiveRef.current.position.setY(-0.2);
     }
   });
 
   return (
-    <mesh ref={meshRef as MutableRefObject<Mesh>}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="white" />
-    </mesh>
+    <>
+      <primitive ref={primitiveRef} object={scene} scale={1} />
+    </>
   );
 }
 
@@ -44,7 +88,8 @@ export default function CenterPiece() {
       <ambientLight intensity={0.1} />
       <pointLight position={[10, 10, 10]} />
       <PerspectiveCamera makeDefault fov={75} position={[0, 0, 5]} />
-      <Box />
+      {/* <Box /> */}
+      <PlaystationDualshock />
     </Canvas>
   );
 }
